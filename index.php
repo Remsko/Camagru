c2c2ad9cf4958755f240f49c0622d98d63db320a
Remsko <pinoit.remi@gmail.com>
2019-01-11 19:36:07 +0100

    meta: footer and mandatory files

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-	echo "Hello World !\n"
-?>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/style/footer.css">
+		<title>Camagru</title>
+	</head>
+	<body>
+		<div>
+			<?php include 'layout/menu.php';?>
+		</div>
+		<div>
+		</div>
+		<div>
+			<?php include 'layout/footer.php';?>
+		</div>
+	</body>
+</html>
---
+++ layout/footer.php
@@ -0,0 +1,2 @@
+<div class="footer" style="width=200;">
+</div>
