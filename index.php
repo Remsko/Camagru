cf9d940b1e36e1b0a38a0221eb91440a85cbb442
Remi PINOIT <rpinoit@e2r10p12.42.fr>
2019-01-13 18:55:55 +0100

    empty commit

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 	<head>
 		<link rel="stylesheet" type="text/css" href="/style/body.css">
 		<link rel="stylesheet" type="text/css" href="/style/footer.css">
-		<link rel="stylesheet" type="text/css" href="/style/menu.css">
+		<link rel="stylesheet" type="text/css" href="/style/header.css">
+		<meta charset="utf-8"/>
 		<title>Camagru</title>
 	</head>
 	<body class="body">
 		<div>
-			<?php include 'layout/menu.php';?>
+			<?php include("layout/header.php");?>
 		</div>
 		<div>
 		</div>
 		<div>
-			<?php include 'layout/footer.php';?>
+			<?php include("layout/footer.php");?>
 		</div>
 	</body>
 </html>
