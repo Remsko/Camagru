56b87297c49eeca68b78a3a54955bcb5409d6569
Remsko <pinoit.remi@gmail.com>
2019-01-14 13:30:59 +0100

    mandatory basic database config

---
+++ config/database.php
@@ -0,0 +1,5 @@
+<?php
+    $DB_DSN = "mysql:host=localhost;charset=UTF8";
+    $DB_USER = "root";
+    $DB_PASSWORD = "";
+?>
---
+++ config/setup.php
@@ -0,0 +1,16 @@
+<?php
+    include("database.php");
+
+    // Connexion to database
+    try {
+        $db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //foreach($db->query('SELECT * from FOO') as $row) {
+        //    print_r($row);
+        //}
+        //$db = null;
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage() . "<br/>";
+        die();
+    }
+?>
