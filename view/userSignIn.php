2400c4f9ab7de677d7967d5c97911d34b6f3d922
Remi PINOIT <rpinoit@e2r11p20.42.fr>
2019-03-10 18:46:21 +0100

    sign in container

---
+++ view/userSignIn.php
@@ -0,0 +1,14 @@
+<div id="container">
+    <div><h1>Sign in</h1><div>
+    <div id="block">
+        <form method="post" action="index.php?page=sign-in">
+            <span>Username:<span><br />
+            <input type="text" name="username" value="" /><br />
+
+            <span>Password:</span><br />
+            <input type="password" name="password" value="" /><br />
+
+            <input type="submit" name="submit" value="OK">
+        </form>
+    </div>
+</div>
