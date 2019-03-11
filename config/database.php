54e32e081b41c4f6300e795a2714187bd1e60f70
Remi PINOIT <rpinoit@e2r10p23.42.fr>
2019-03-11 16:09:20 +0100

    db connection

--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
 <?php
-	$DB_DSN = "mysql:host=127.0.0.1;dbname=Camagru;charset=utf8";
+    $DB_DSN = "mysql:host=127.0.0.1";
+    $DB_NAME = "dbname=CAMAGRU";
     $DB_USER = "root";
     $DB_PASSWORD = "dbroot";
 ?>
