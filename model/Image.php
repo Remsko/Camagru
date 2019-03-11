8dbf215b722b17eeae75c14ae99102f770a9950f
mabessir <mabessir@student.42.fr>
2019-03-11 17:48:02 +0100

    New Image Class

---
+++ model/Image.php
@@ -0,0 +1,86 @@
+<?php
+	include("picmanager.php");
+
+	class Image {
+
+		private $_id,
+				$_username,
+				$_name,
+				$_type,
+				$_descrip,
+				$_blob;
+
+		public function __construct(array $donnees)
+		{
+			$this->hydrate($donnees);
+		}
+		public function hydrate(array $donnees)
+		{
+			foreach ($donnees as $key => $value)
+			{
+				$method = 'set'.ucfirst($key);
+				if (method_exists($this, $method))
+				{
+					$this->$method($value);
+				}
+			}
+		}
+
+		public function id() { return $this->_id; }
+		public function name() {return $this->_name;}
+		public function username() {return $this->_username;}
+		public function type() {return $this->_type;}
+		public function descrip() {return $this->_descrip;}
+		public function blob() {return $this->_blob;}
+
+		public function setId($id)
+		{
+			if (is_int($id))
+			{
+				$this->_id = $id;
+			}
+		}
+
+		public function setName($name) 
+		{
+			if (is_string($name))
+			{
+				$this->_name = $name;
+			}
+		}
+
+		public function setUsername($username) 
+		{
+			if (is_string($username))
+			{
+				$this->_username = $username;
+			}	
+		}
+		
+		public function setType($type)
+		{
+			$extensions = array('/png', '/jpg', '/jpeg');
+			$extension = strrchr($type, '/');
+			if(!in_array($extension, $extensions)) {
+				echo 'Wrong file type. You can only upload png jpg or jpeg files.';
+			}
+			else {
+				$this->_type = $type;
+			}
+		}
+
+		public function setDescrip($descrip)
+		{
+			if (is_string($descrip))
+			{
+				$this->_descrip = $descrip;
+			}
+		}
+
+		public function setBlob($path)
+		{
+			$blob = file_get_contents($path);
+			$this->_blob = $blob;
+		}
+	}
+?>
