6ff28402b81a72b8ed0050567fbb933099ce1839
mabessir <mabessir@student.42.fr>
2019-03-11 17:56:04 +0100

    Normed

--- model/Image.php
+++ model/Image.php
@@ -3,62 +3,68 @@
 
 	class Image {
 
-		private $_id,
-				$_username,
-				$_name,
-				$_type,
-				$_descrip,
-				$_blob;
+		private $_id;
+		private	$_username;
+		private	$_name;
+		private	$_type;
+		private	$_descrip;
+		private	$_blob;
 
-		public function __construct(array $donnees)
-		{
+		public function __construct(array $donnees) {
 			$this->hydrate($donnees);
 		}
-		public function hydrate(array $donnees)
-		{
-			foreach ($donnees as $key => $value)
-			{
+		public function hydrate(array $donnees) {
+			foreach ($donnees as $key => $value) {
 				$method = 'set'.ucfirst($key);
-				if (method_exists($this, $method))
-				{
+				if (method_exists($this, $method)) {
 					$this->$method($value);
 				}
 			}
 		}
 
-		public function id() { return $this->_id; }
-		public function name() {return $this->_name;}
-		public function username() {return $this->_username;}
-		public function type() {return $this->_type;}
-		public function descrip() {return $this->_descrip;}
-		public function blob() {return $this->_blob;}
+		public function GetId() {
+			return $this->_id;
+		}
 
-		public function setId($id)
-		{
-			if (is_int($id))
-			{
+		public function GetName() {
+			return $this->_name;
+		}
+
+		public function GetUsername() {
+			return $this->_username;
+		}
+
+		public function GetType() {
+			return $this->_type;
+		}
+
+		public function GetDescrip() {
+			return $this->_descrip;
+		}
+
+		public function GetBlob() {
+			return $this->_blob;
+		}
+
+		public function setId($id) {
+			if (is_int($id)) {
 				$this->_id = $id;
 			}
 		}
 
-		public function setName($name) 
-		{
-			if (is_string($name))
-			{
+		public function setName($name) {
+			if (is_string($name)){
 				$this->_name = $name;
 			}
 		}
 
-		public function setUsername($username) 
-		{
-			if (is_string($username))
-			{
+		public function setUsername($username) {
+			if (is_string($username)){
 				$this->_username = $username;
 			}	
 		}
 		
-		public function setType($type)
-		{
+		public function setType($type) {
 			$extensions = array('/png', '/jpg', '/jpeg');
 			$extension = strrchr($type, '/');
 			if(!in_array($extension, $extensions)) {
@@ -69,18 +75,15 @@
 			}
 		}
 
-		public function setDescrip($descrip)
-		{
-			if (is_string($descrip))
-			{
+		public function setDescrip($descrip) {
+			if (is_string($descrip)) {
 				$this->_descrip = $descrip;
 			}
 		}
 
-		public function setBlob($path)
-		{
+		public function setBlob($path) {
 			$blob = file_get_contents($path);
 			$this->_blob = $blob;
 		}
-	}
+}
 ?>
