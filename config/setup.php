46daf9777e0d8274f49e2cfe91393b5cdbfcd8aa
Remsko <pinoit.remi@gmail.com>
2019-03-13 20:38:29 +0100

    router

--- config/setup.php
+++ config/setup.php
@@ -2,7 +2,7 @@
     include("database.php");
 
     try {
-		$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+		$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD, '');
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$newDb = "CREATE DATABASE IF NOT EXISTS CAMAGRU DEFAULT CHARACTER SET utf8"; 
 		$db->exec($newDb);
@@ -12,26 +12,26 @@
         die();
 	}
 	try {
-		$db = new PDO($DB_DSN. ';' .$DB_NAME, $DB_USER, $DB_PASSWORD);
+		$db = new PDO($DB_DSN. ';' .$DB_NAME, $DB_USER, $DB_PASSWORD, '');
 		$newUsersTable = "CREATE TABLE IF NOT EXISTS Users(
 			id smallint(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 			username VARCHAR(15) NOT NULL,
 			mail VARCHAR(30) NOT NULL,
 			password VARCHAR(150) NOT NULL
-	)";
-	$db->exec($newUsersTable);
-	$newImagesTable = "CREATE TABLE IF NOT EXISTS Images(
-		id smallint(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-		name VARCHAR(15) NOT NULL,
-	    username VARCHAR(50) NOT NULL,
-	    type VARCHAR(25) NOT NULL,
-	    descrip text NOT NULL,
-	    img_blob longblob NOT NULL
+		)";
+		$db->exec($newUsersTable);
+		$newImagesTable = "CREATE TABLE IF NOT EXISTS Images(
+			id smallint(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+			name VARCHAR(15) NOT NULL,
+	    	username VARCHAR(50) NOT NULL,
+	    	type VARCHAR(25) NOT NULL,
+	    	descrip text NOT NULL,
+	    	img_blob longblob NOT NULL
 		)";
 		$db->exec($newImagesTable);
 	}
 	catch (PDOException $e) {
 		echo "Connection failed (filling): " . $e->getMessage();
 		die();
-}
+	}
 ?>
---
+++ controllers/Router.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class Router {
+    private $_controller;
+    private $_view;
+
+    public function routeRequest() {
+        try {
+            spl_autoload_register(function($class) {
+                require_once('models/'.$class.'.class.php');
+            });
+
+            $url = '';
+            if (isset($_GET['url'])) {
+                $urlVariables = filter_var($_GET['url']);
+                $url = explode('/', $urlVariables, FILTER_SANITIZE_URL);
+            
+                $controller = ucfirst(strtolower($url[0]));
+                $controllerClass = 'Controller'.$controller;
+                $controllerFile = 'controllers/'.$controllerClass.'.class.php';
+            
+                if (file_exists($controllerFile)) {
+                    require_once($controllerFile);
+                    $this->_controller = new $controllerClass($url);
+                }
+                else {
+                    throw new Exception('Page not found');
+                }
+            }
+            else {
+                require_once('controllers/ControllerHome.php');
+                $this->_controller = new ControllerHome($url);
+            }
+        }
+        catch (Exception $e) {
+            $errorMsg = $e->getMessage();
+            require_once('views/error/error.php');
+        }
+    }
+}
