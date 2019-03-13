dce46b26468cd3878e1e0005b2ef65abcbbe2792
Rémi Pinoit <33517098+Remsko@users.noreply.github.com>
2019-03-13 13:51:37 +0100

    Update Image.php

--- model/Image.php
+++ model/Image.php
@@ -13,6 +13,7 @@
 		public function __construct(array $donnees) {
 			$this->hydrate($donnees);
 		}
+		
 		public function hydrate(array $donnees) {
 			foreach ($donnees as $key => $value) {
 				$method = 'set'.ucfirst($key);
