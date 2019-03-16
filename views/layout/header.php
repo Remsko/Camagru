b7a9a6dd926a8645210d012ba2686e6b6b11ae50
Remi PINOIT <rpinoit@e2r11p23.42.fr>
2019-03-16 14:00:29 +0100

    modify links

--- views/layout/header.php
+++ views/layout/header.php
@@ -1,8 +1,8 @@
 <div class="header-div">
     <span>Camagru<span>
     <ul>
-        <li><a href="" title="Gallery">Gallery</a></li>
-        <li><a href="user/signin" title="Sign in">Sign in</a></li>
-        <li><a href="user/signup" title="sign up">Sign Up</a></li>
+        <li><a href="/" title="Gallery">Gallery</a></li>
+        <li><a href="/user/signin" title="Sign in">Sign in</a></li>
+        <li><a href="/user/signup" title="sign up">Sign Up</a></li>
     </ul>
 </div>
