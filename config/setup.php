e5c4eefe8f44cc9972955b4b0f50192ba3390d28
Remi PINOIT <rpinoit@e2r11p23.42.fr>
2019-03-17 15:21:40 +0100

    refact nested shity try catch

--- config/setup.php
+++ config/setup.php
@@ -5,22 +5,13 @@
 		$db = new PDO($DB_DSN.';'.$DB_NAME, $DB_USER, $DB_PASSWORD, []);
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$db->exec('DROP DATABASE camagru');
-	} 
-	catch (PDOException $e) {
-		try {
-			$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
-		}
-		catch (PDOException $e) {
-			echo "Connection failed (creation): " . $e->getMessage();
-			die();
-		}
-	}
-	try {
+
+		$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
 		$newTables = file_get_contents('config/setup.sql');
 		$db->exec($newTables);
-	}
+	} 
 	catch (PDOException $e) {
-		echo "Connection failed (filling): " . $e->getMessage();
+		echo "Connection failed: " . $e->getMessage();
 		die();
 	}
 	
