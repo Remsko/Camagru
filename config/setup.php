b1f97e25bac3e773def7b776d9f34cce07ed87e1
Remi PINOIT <rpinoit@e2r11p23.42.fr>
2019-03-17 15:23:57 +0100

    setup fix

--- config/setup.php
+++ config/setup.php
@@ -2,11 +2,8 @@
     include("database.php");
 
     try {
-		$db = new PDO($DB_DSN.';'.$DB_NAME, $DB_USER, $DB_PASSWORD, []);
+		$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD, []);
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$db->exec('DROP DATABASE camagru');
-
-		$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
 		$newTables = file_get_contents('config/setup.sql');
 		$db->exec($newTables);
 	} 
