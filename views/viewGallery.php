1076472229631311ee142862e4ea9ac6639db306
Remsko <pinoit.remi@gmail.com>
2019-03-27 23:17:21 +0100

    next and prec page

--- views/viewGallery.php
+++ views/viewGallery.php
@@ -12,8 +12,14 @@
             require('layout/commentForm.php');
         }
     }
+    if ($currentPage != 1) {
+        echo '<a href="index.php?page='.($currentPage - 1).'"><<</a>';
+    }
     for ($i = 1; $i <= $pagesTotal; $i++) {
         echo '<a href="/index.php?page='.$i.'">'.$i.'</a>';
+    }
+    if ($currentPage != $pagesTotal) {
+        echo '<a href="index.php?page='.($currentPage + 1).'">>></a>';
     }
 ?>
 <h3>That's it ! There is no more pictures !</h3>
