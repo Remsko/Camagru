fe415d334f60cd5e844449f3b71f53089939e18c
mabessir <mabessir@student.42.fr>
2019-04-01 18:51:44 +0200

    it works. update on camera.js and view studio.

--- views/viewStudio.php
+++ views/viewStudio.php
@@ -3,9 +3,14 @@
 <?php
     if (isset($_SESSION['userId'])) {
 		require_once('layout/camera.php');
-		foreach ($images as $image) {
+?>
+<div id='imgContainer'>
+<?php		foreach ($images as $image) {
 			require('layout/oldpics.php');
 		}
+?>
+</div>
+<?php
     }
     else {
         echo '<font color="red">'.'You must be logged in to access the studio.'."</font>";
