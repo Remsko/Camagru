105bf87b23472205d283b518cd7062778cb9abc4
Mendy <mabessir@student.42.fr>
2019-04-03 22:11:15 +0200

    Modified form.

--- views/layout/camera.php
+++ views/layout/camera.php
@@ -7,8 +7,9 @@
 <video id="video"></video>
 <button id="startbutton" disabled>Take Picture</button>
 <form enctype="multipart/form-data" action="studio/uploadImage" method="post">
+	<input type="hidden" name="MAX_FILE_SIZE" value="2500000"/>
 	<label for="file">Select your own picture :</label>
-	<input type="file" id="picturePath" name="picture" size=500000 accept="image/*"/>
+	<input type="file" id="picturePath" name="picture" size=50 accept="image/*"/>
 	<input type="submit" value="Send" />
 </form>
 <div id='picture'>
